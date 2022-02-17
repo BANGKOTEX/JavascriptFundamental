5e6ecedb57fd20debe5dc82a32c0aa639a395a38
BANGKOTEX <99166127+BANGKOTEX@users.noreply.github.com>
2022-02-17 14:16:32 +0700

    membuat tipe data

---
+++ TipeData.js
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>JavaScript</h2>
+
+<p>JavaScript mengevaluasi ekspresi dari kiri ke kanan. Urutan yang berbeda dapat menghasilkan hasil yang berbeda:</p>
+
+<p id="demo"></p>
+
+<script>
+let x = 16 + 4 + "BANGKOTEX";
+document.getElementById("demo").innerHTML = x;
+</script>
+
+</body>
+</html>
