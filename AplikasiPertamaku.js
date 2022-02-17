3e76afc75a636abae0e93736611e0c93da05160d
BANGKOTEX <99166127+BANGKOTEX@users.noreply.github.com>
2022-02-17 14:02:42 +0700

    membuat aplikasi pertama

---
+++ AplikasiPertamaku.js
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>Ini Aplikasi Javasript Pertamaku</h2>
+
+<p id="demo">JavaScript dapat mengubah konten HTML.</p>
+
+<button type="button" onclick='document.getElementById("demo").innerHTML = "Hello Ini Adalah Aplikasi JavaScript! Pertamaku"'>Click Me!</button>
+
+</body>
+</html>
