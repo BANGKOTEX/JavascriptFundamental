5b6625533e5757926631c755191dd42bb24a469b
BANGKOTEX <99166127+BANGKOTEX@users.noreply.github.com>
2022-02-17 14:14:42 +0700

    membuat aritmatika

---
+++ Aritmatika.js
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>JavaScript Arithmetic</h2>
+<p>A operasi aritmatika khas membutuhkan dua angka dan menghasilkan angka baruoperasi aritmatika khas membutuhkan dua angka dan menghasilkan angka baru.</p>
+
+<p id="demo"></p>
+
+<script>
+let x = 100 + 50;
+document.getElementById("demo").innerHTML = x;
+</script>
+
+</body>
+</html>
